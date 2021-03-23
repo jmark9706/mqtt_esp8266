b098e99cdb36a4458d723f4f0c4557db6d4be336
jmark9706 <1157024+jmark9706@users.noreply.github.com>
2021-03-23 12:26:38 -0500

    updated with callback, with LED on pines 12, 13

--- mqtt_esp8266.ino
+++ mqtt_esp8266.ino
@@ -16,6 +16,7 @@
        http://arduino.esp8266.com/stable/package_esp8266com_index.json
   - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
   - Select your ESP8266 in "Tools -> Board"
+   Has LED output on pins 12, 13
 */
 
 #include <ESP8266WiFi.h>
@@ -23,6 +24,8 @@
 #include <Wire.h>
 #include <SparkFunBME280.h>
 
+#define  BLUE 12
+#define YELLOW 13
 // Update these with values suitable for your network.
 
 const char* ssid = "cirrus";
@@ -34,7 +37,8 @@ const char* PUB_TOPIC = "environ/test";
  float baro_f;
  char msg[70];
  char * mp = msg;
-
+ char uarr[40];
+ int uleng;
 WiFiClient espClient;
 PubSubClient client(espClient);
  BME280 mySensor;
@@ -99,8 +103,10 @@ void callback(char* topic, byte* payload, unsigned int length) {
     digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
     // but actually the LED is on; this is because
     // it is active low on the ESP-01)
+     digitalWrite(BLUE, LOW);
   } else {
     digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+     digitalWrite(BLUE, HIGH);
   }
 
 }
@@ -153,7 +159,7 @@ void read_sensors()
   Serial.println();
 }
 void format_payload(){
-  String humid = String(humidity);
+      String humid = String(humidity);
       int hleng = humid.length() + 1;
       char harr[hleng];
       humid.toCharArray(harr,hleng);
@@ -177,9 +183,11 @@ void format_payload(){
      strcat (mp, barr);
      strcat (mp," mbar");
 }
-
+// SETUP starts here ===================================
 void setup() {
   pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  pinMode(BLUE, OUTPUT);
+  pinMode (YELLOW, OUTPUT);
   Serial.begin(115200);
   setup_wifi();
   setup_I2C();
@@ -188,21 +196,23 @@ void setup() {
 }
 
 void loop() {
-
+  digitalWrite(YELLOW, HIGH);
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
 
   unsigned long now = millis();
-  if (now - lastMsg > 2000) {
+  if (now - lastMsg > 5000) {
     lastMsg = now;
     ++value;
    // snprintf (msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
+   digitalWrite(YELLOW, LOW);
    read_sensors();
    format_payload();
     Serial.print("Publish message: ");
     Serial.println(mp);
     client.publish(PUB_TOPIC, mp);
+    
   }
 }
